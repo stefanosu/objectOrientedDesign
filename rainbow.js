b7e1601cf9161030dee46af4732e391dc8c4fc8e
stefanosu <stefanosu422@gmail.com>
2016-05-30 20:49:58 -0400

    adding project using object oriented design

---
+++ rainbow.js
@@ -0,0 +1,25 @@
+var Rainbow = function(x, y, radius){
+       this.x = x;
+       this.y = y;
+       this.radius = radius;
+};
+    var lowRainbow = new Rainbow(140, 250, 300);
+    var highRainbow = new Rainbow(250, 350, 450);
+
+var drawRainbow = function(rainbow) {
+    noFill();
+    strokeWeight(5);
+    stroke(255, 0, 0);
+    arc(rainbow.x, rainbow.y, rainbow.radius, rainbow.radius, 180, 360);
+    stroke(250, 200, 0);
+    arc(rainbow.x, rainbow.y, rainbow.radius-10, rainbow.radius-10, 180, 360);
+    stroke(240, 255, 102);
+    arc(rainbow.x, rainbow.y, rainbow.radius-20, rainbow.radius-20, 180, 360);
+    stroke(29, 255, 13);
+    arc(rainbow.x, rainbow.y, rainbow.radius-30, rainbow.radius-30, 180, 360);
+    stroke(122, 155, 255);
+    arc(rainbow.x, rainbow.y, rainbow.radius-40, rainbow.radius-40, 180, 360);
+};
+
+drawRainbow(highRainbow);
+drawRainbow(lowRainbow);
